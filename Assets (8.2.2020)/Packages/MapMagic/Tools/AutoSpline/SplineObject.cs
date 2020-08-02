0c329ab1effd46e33b2277ef89f1bc2b4c9807f0
rim0703 <rim0703@163.com>
2020-08-02 23:03:24 +0900

    구르음이 생겼어요

---
+++ Assets (8.2.2020)/Packages/MapMagic/Tools/AutoSpline/SplineObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Den.Tools.AutoSplines
+{
+	public class SplineObject : MonoBehaviour
+	{
+		public Spline[] splines = new Spline[] { new Spline( new Vector3(0,0,0), new Vector3(100,0,100) ) };
+
+		public bool drawBeizer = false;
+		public bool drawLines = true;
+		public bool drawNodes = false;
+	}
+}
---
+++ Assets (8.2.2020)/Scenes/Main/World/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    public float smoothSpeed;
+    public Vector3 offset;
+    public Vector3 lookatOffset;
+
+    void Start()
+    {
+
+    }
+
+    // Update after frame is called
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = transform.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(target.TransformPoint(offset), desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(target);
+    }
+}
