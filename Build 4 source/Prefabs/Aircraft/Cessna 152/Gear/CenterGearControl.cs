28e4239fe20000bc1fd03caede3f3285749b8f1b
Ju Won <juwonpee@gmail.com>
2020-08-02 16:32:26 +0900

    Build 4 & 2

---
+++ Build 4 source/Prefabs/Aircraft/Cessna 152/Gear/CenterGearControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterGearControl : MonoBehaviour
+{
+    // global variables
+    public GameObject fusalage;
+    public FusalageControl fusalageScript;
+    public WheelCollider centerGear;
+
+    public float maxSteerAlpha;
+    public float steerAlpha;
+    float maxBrakeForce;
+    float brakeForce;
+    float smoothing;
+
+    // private
+    float yAxis;
+
+    float calcDefAlpha (float yAxis, float maxSteerAlpha)
+    {
+        return (yAxis / 100f) * maxSteerAlpha;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get global values
+        fusalageScript = fusalage.GetComponent<FusalageControl>();
+        centerGear = fusalageScript.centerGear;
+        maxSteerAlpha = fusalageScript.gearMaxSteerAlpha;
+        maxBrakeForce = fusalageScript.gearMaxBrakeForce;
+        smoothing = fusalageScript.gearSmoothing;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // general housekeeping
+        yAxis = fusalageScript.getRudder();
+
+        // rotate z axis of gear
+        steerAlpha = calcDefAlpha(yAxis, maxSteerAlpha);
+        centerGear.steerAngle = Mathf.SmoothStep(centerGear.steerAngle,steerAlpha, smoothing * Time.deltaTime);
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Flight-simulator
-Build 1 (7.23.2020)
-Built with Unity version 2019.4.4f1
+Build 4 (8.2.2020) built with Unity version 2019.4.6f1
+
+Build 1 (7.23.2020) built with Unity version 2019.4.4f1
